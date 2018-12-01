d0dfbd22ce37f7d8321f8ee378b5fd0731a3c0d0
nikhilogic <nikhilogic@outlook.com>
2018-12-01 01:06:21 -0500

    library update build for gitpages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Ngrid2Module } from 'ngrid2';
 
 import { AppComponent } from './app.component';
-import { Ngrid2Component } from 'ngrid2';
 @NgModule({
   declarations: [
-    AppComponent,    
-    Ngrid2Component
+    AppComponent    
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    Ngrid2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
